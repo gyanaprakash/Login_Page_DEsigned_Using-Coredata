0278da6eed574bf0b4772fcd1a97b81f9649524b
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2015-01-10 10:58:20 +0530

    Initial Commit

---
+++ YAAC_mission/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YAAC_mission
+//
+//  Created by Bsetecip10 on 10/01/15.
+//  Copyright (c) 2015 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YAAC_mission/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YAAC_mission
+//
+//  Created by Bsetecip10 on 10/01/15.
+//  Copyright (c) 2015 gyana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
